37b0bd3df3dc8901d68177bf98c44e779859bc52
enajrevir <enajrevir@gmail.com>
2022-04-23 20:15:38 +0900

    jsx to element 변환 question

---
+++ case1_jsx-and-react-element/index.js
@@ -0,0 +1,14 @@
+const React = {
+  createElement,
+}
+
+// 런타임 시 각 Node를 트랜스파일러인 Babel에 알려주기 위해 참조한다.
+const element = (
+  <div>
+    <h1>Hello World</h1>
+  </div>
+)
+
+const container = document.getElementById('root')
+
+container.innerHTML = prettyPrintJson.toHtml(React.createElement(element))
